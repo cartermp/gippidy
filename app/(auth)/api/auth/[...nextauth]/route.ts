1d7672c20cf9135f91c4f6278f93efc4d39534d3
Phillip Carter <pcarter@fastmail.com>
2025-05-31 11:31:22 -0700

    google auth checkpoint

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,24 @@
-export { GET, POST } from '@/app/(auth)/auth';
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import type { User } from 'next-auth';
+
+const authOptions = {
+  providers: [
+    Google({
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      clientId: process.env.GOOGLE_ID!,
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }: { user: User }) {
+      const allowedEmail = process.env.ALLOWED_EMAIL?.trim();
+      const userEmail = user.email?.trim();
+      return allowedEmail === userEmail;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
