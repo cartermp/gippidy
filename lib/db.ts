a8093f5633f9339db470a289259f067c302a0b83
Phillip Carter <pcarter@fastmail.com>
2026-03-16 10:42:24 -0700

    sharing works?

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,8 @@
 import { Pool } from 'pg';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var _pgPool: Pool | undefined;
-}
-
-const pool = global._pgPool ?? new Pool({ connectionString: process.env.POSTGRES_URL });
-if (process.env.NODE_ENV !== 'production') global._pgPool = pool;
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 export const query = (sql: string, params?: unknown[]) => pool.query(sql, params);
