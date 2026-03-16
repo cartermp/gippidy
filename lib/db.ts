7d9085901c2a767517495cae976f5c0f62c63711
Phillip Carter <pcarter@fastmail.com>
2026-03-16 10:50:36 -0700

    lol

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,8 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
 export const query = (sql: string, params?: unknown[]) => pool.query(sql, params);
-DATABASE_URL
