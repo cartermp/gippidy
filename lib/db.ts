185cb8f65a704eb2788f44a579829b19e33228af
Phillip Carter <pcarter@fastmail.com>
2026-03-18 10:26:58 -0700

    no more debug

--- lib/db.ts
+++ lib/db.ts
@@ -5,26 +5,4 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false },
 });
 
-export const query = async (sql: string, params?: unknown[]) => {
-  const debug1 = await pool.query(`
-  SELECT
-    current_database() AS db,
-    current_user AS usr,
-    current_schema() AS schema,
-    current_setting('search_path') AS search_path
-`);
-
-// console log the result
-  console.log('DB DEBUG 1', debug1.rows);
-
-const debug2 = await pool.query(`
-  SELECT
-    schemaname,
-    tablename
-  FROM pg_tables
-  WHERE tablename = 'rate_limits'
-`);
-
-console.log('DB DEBUG 2', debug2.rows);
-  return pool.query(sql, params);
-};
+export const query = async (sql: string, params?: unknown[]) => pool.query(sql, params);
