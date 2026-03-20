cbbf99ce46224e81afacfdf7aa28ea68b6d9931f
Phillip Carter <pcarter@fastmail.com>
2026-03-19 18:02:31 -0700

    sql log

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: { rejectUnauthorized: true },
 });
 
 export const query = async (sql: string, params?: unknown[]) => pool.query(sql, params);
